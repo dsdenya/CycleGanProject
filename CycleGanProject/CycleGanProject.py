6a3551b6ef8a4c0ce30f484f4392e75006383d01
Shoya Dixon <SDixon@SophieDavis.local>
2024-06-07 16:16:42 -0400

    This was the 'before' optimization, but with comments for the future

--- CycleGanProject/CycleGanProject.py
+++ CycleGanProject/CycleGanProject.py
@@ -11,41 +11,11 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
 
-# The origins of the features (how the dataset was parsed in the first place) is shown in the past commit which was this:
-#for raw_record in monet_dataset.take(10):
-#      monet = tf.train.Example()
-#      monet.ParseFromString(raw_record.numpy())
-      
-#for photo_raw_record in photo_dataset.take(10):
-#      photo = tf.train.Example()
-#     photo.ParseFromString(photo_raw_record.numpy())
-# That's how we got the features below but since i know what it is now..just took it out
-
-def decode_fn(record_bytes):
-    features = {
-        'image': tf.io.FixedLenFeature([], tf.string),
-        'image_name': tf.io.FixedLenFeature([], tf.string),
-        'target': tf.io.FixedLenFeature([], tf.string),
-    }
-    parsed_features = tf.io.parse_single_example(record_bytes, features) 
-    image = tf.image.decode_jpeg(parsed_features['image'], channels=3)
-    return image
-
-def preprocess_image(image):
-    # Convert image to float32 and normalize because it was originally Uint8 
-    image = tf.cast(image, tf.float32) / 255.0
-    return image
-
-def load_and_preprocess_dataset(file_paths):
-    dataset = tf.data.TFRecordDataset(file_paths)
-    dataset = dataset.map(decode_fn)
-    dataset = dataset.map(preprocess_image)
-    return dataset.batch(3)
-
 if __name__== "__main__":
     cleaned_dir = r'GAN_tf_records/records/cleaned'
     if not os.path.isdir(cleaned_dir):
         os.makedirs(cleaned_dir)
+    
     
     monet_dirpath = r'CycleGanProject\GAN_tf_records\records\monet_tfrec'
     photo_dirpath = r'CycleGanProject\GAN_tf_records\records\photo_tfrec'
@@ -54,13 +24,61 @@ if __name__== "__main__":
     print('Monet # of files:' , len(monet_file))
     print('Photo # of files:' , len(photo_file))
  
-    monet_normalized = load_and_preprocess_dataset(monet_file)
-    photo_normalized = load_and_preprocess_dataset(photo_file)
+    monet_dataset = tf.data.TFRecordDataset(monet_file) #Creates a tensorflow Dataset comprising records from one or more TFRecord files.
+    photo_dataset = tf.data.TFRecordDataset(photo_file)
 
-# ****My personal for the future refresher summary notes****: 
-# So, in the load_and_preprocess_dataset, we'recreating a tfrecord dataset from the file paths. 
-# Then in dataset = dataset.map(decode_fn), we are taking the dataset contents passing that to decode_fn to get specifically the 
-# image feature (that is now an an image tensor btw). And then once we have the image we normalize it in dataset.dataset.map(preprocess_image) and
-# then we return batches or groups of the images.
-# The previous code was "This is the 'before' code. In this code, I had memory allocation issues that were exceeding the memory by 10%. 
-# This was occuring within the  decode_fn and tensor_normalization functions. The next priority to optimize this code is this so it doesn't exceed memory problems again...
+
+    #An Example is a standard proto storing data for training and inference. Also, this will get the 'features' of the tfrecord so that you can parse it
+    #Iterates through the first 10 records of each dataset, parsing them into tf.train.Example objects to inspect their contents.
+    for raw_record in monet_dataset.take(10):
+      monet = tf.train.Example()
+      monet.ParseFromString(raw_record.numpy())
+      
+    for photo_raw_record in photo_dataset.take(10):
+      photo = tf.train.Example()
+      photo.ParseFromString(photo_raw_record.numpy())
+
+    #This is where the features will go so that we can parse the images.
+    #The decode_fn function extracts the image field from each record. This field contains JPEG-encoded image data.
+    #The JPEG data is then decoded into an image tensor using tf.image.decode_jpeg within the tensor_normalization function
+  
+    def decode_fn(record_bytes):
+        features = {
+            'image': tf.io.FixedLenFeature([], tf.string),
+            'image_name': tf.io.FixedLenFeature([], tf.string),
+            'target': tf.io.FixedLenFeature([], tf.string),
+        }
+        return tf.io.parse_single_example(record_bytes, features)
+    
+    #Creates a map that attaches the features to values. Like a dictionary.
+    #Uses the map function to apply decode_fn to each element of the Monet and photo datasets, 
+    #effectively parsing the records into dictionaries with the expected features.
+
+    monet_dataset_map = monet_dataset.map(decode_fn)
+    photo_dataset_map = photo_dataset.map(decode_fn)
+
+    type(monet_dataset_map)
+    
+    
+    #Make a normalization and tensor function and put it in a list and return it.
+    #tensor_normalization function takes a dataset of parsed records, decodes the JPEG image, casts it to tf.float32, and normalizes it.
+    #The function returns a new dataset of normalized image tensors.
+    #Within the tensor_normalization function, the features['image'] contains the JPEG image data as a serialized string.
+    #The tf.image.decode_jpeg(features['image'].numpy()) function call decodes the serialized string back into an image tensor..
+    def tensor_normalization(dataset_map):
+      images_normalized = []
+      for features in dataset_map:
+
+        tensor = tf.image.decode_jpeg(features['image'].numpy())
+        tensor = tf.cast(tensor, tf.float32) # Since the original type of image_tensor was uint8, we need to change it to tf.float32 so it can be compatible to perform arithemitc with 127.5
+        normalized = (tensor - np.amin(tensor))/ (np.amax(tensor) - np.amin(tensor)) #Normalization formula
+        images_normalized.append(normalized)
+      
+      images_normalized_dt = tf.data.Dataset.from_tensor_slices(images_normalized)
+      return images_normalized_dt.batch(3)
+      #return tf.data.Dataset.from_tensor_slices(images_normalized)
+    #Applies the normalization function to both Monet and photo datasets, resulting in monet_normalized
+    #and photo_normalized datasets containing normalized image tensors.So, basically, we have two 
+    #TensorFlow datasets (monet_normalized and photo_normalized) containing normalized image tensors derived from the original TFRecord files.  
+    monet_normalized = tensor_normalization(monet_dataset_map)
+    photo_normalized = tensor_normalization(photo_dataset_map)
