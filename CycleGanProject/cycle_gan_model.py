6f8595d61ec692b90bc706b835121a486a1371c7
Shoya Dixon <SDixon@SophieDavis.local>
2024-06-11 16:32:40 -0400

    Typos

--- CycleGanProject/cycle_gan_model.py
+++ CycleGanProject/cycle_gan_model.py
@@ -1,29 +1,24 @@
-#Generator
-#Composed of three parts: Encode, transformer, and decoder
-
-
 import torch
 import torch.nn as nn 
 from CycleGanProject import load_and_preprocess_dataset
 
+#Two Generators
+#Composed of three parts: Encode, transformer, and decoder
 class Generator(nn.Module):
     def __init__(self, input_channel:int, output_channel:int, kernel_size:int):
         
         super().__init__()
     
-    conv_layer = nn.Sequential (
-         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size)
-         self.rel1 = nn.ReLu(),
-         
-         self.conv2 = nn.Conv2d(input_channel, output_channel, kernel_size, batch_size)
-         self.rel2 = nn.ReLu()
-         self.pool2 = nn.MaxPool2d(kernel_size)
+        self.conv_layer = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size),
+                nn.ReLU(),
+                
+                nn.Conv2d(input_channel, output_channel, kernel_size),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size),
 
-         self.conv3 = nn.Conv2d(input_channel, output_channel, kernel_size, batch_size)
-         self.rel3 = nn.ReLu()
-         self.pool3 = nn.MaxPool2d(kernel_size)
-
-
-
- )
+                nn.Conv2d(input_channel, output_channel, kernel_size),
+                nn.ReLU(),
+                nn.MaxPool2d(kernel_size)
+    )
     
