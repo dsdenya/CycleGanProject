524eb78a98b461c902ffbed5d0e66d056312e0f3
Shoya Dixon <SDixon@SophieDavis.local>
2024-06-13 19:08:44 -0400

    Updated: Discriminator, Generator Added

--- CycleGanProject/CycleGanProject.py
+++ CycleGanProject/CycleGanProject.py
@@ -1,4 +1,4 @@
-#data processing
+# data processing
 
 import tensorflow as tf
 from glob import glob
@@ -9,19 +9,22 @@ from tensorflow.io import FixedLenFeature, VarLenFeature
 import matplotlib.pyplot as plt
 import os
 
+
 def decode_fn(record_bytes):
     features = {
-        'image': tf.io.FixedLenFeature([], tf.string),
-        'image_name': tf.io.FixedLenFeature([], tf.string),
-        'target': tf.io.FixedLenFeature([], tf.string),
+        "image": tf.io.FixedLenFeature([], tf.string),
+        "image_name": tf.io.FixedLenFeature([], tf.string),
+        "target": tf.io.FixedLenFeature([], tf.string),
     }
-    parsed_features = tf.io.parse_single_example(record_bytes, features) 
-    image = tf.image.decode_jpeg(parsed_features['image'], channels=3)
+    parsed_features = tf.io.parse_single_example(record_bytes, features)
+    image = tf.image.decode_jpeg(parsed_features["image"], channels=3)
     return image
+
 
 def preprocess_image(image):
     image = tf.cast(image, tf.float32) / 255.0
     return image
+
 
 def load_and_preprocess_dataset(file_paths):
     dataset = tf.data.TFRecordDataset(file_paths)
@@ -29,31 +32,29 @@ def load_and_preprocess_dataset(file_paths):
     dataset = dataset.map(preprocess_image)
     return dataset.batch(4)
 
-if __name__== "__main__":
-    cleaned_dir = r'GAN_tf_records/records/cleaned'
+
+if __name__ == "__main__":
+    cleaned_dir = r"GAN_tf_records/records/cleaned"
     if not os.path.isdir(cleaned_dir):
         os.makedirs(cleaned_dir)
-    
-    monet_dirpath = r'CycleGanProject\GAN_tf_records\records\monet_tfrec'
-    photo_dirpath = r'CycleGanProject\GAN_tf_records\records\photo_tfrec'
-    monet_file = tf.io.gfile.glob(monet_dirpath + '/*.tfrec')
-    photo_file = tf.io.gfile.glob(photo_dirpath + '/*.tfrec')
-    print('Monet # of files:' , len(monet_file))
-    print('Photo # of files:' , len(photo_file))
- 
+
+    monet_dirpath = r"CycleGanProject\GAN_tf_records\records\monet_tfrec"
+    photo_dirpath = r"CycleGanProject\GAN_tf_records\records\photo_tfrec"
+    monet_file = tf.io.gfile.glob(monet_dirpath + "/*.tfrec")
+    photo_file = tf.io.gfile.glob(photo_dirpath + "/*.tfrec")
+    print("Monet # of files:", len(monet_file))
+    print("Photo # of files:", len(photo_file))
+
     monet_normalized = load_and_preprocess_dataset(monet_file)
     photo_normalized = load_and_preprocess_dataset(photo_file)
 
-#The code used to get the shape of the tensor images
-#Output: shape=(4, 256, 256, 3), dtype=float32 <-- 4 batches, 256 x 256 matrix, 3 channels (rgb)
-    #for element in monet_normalized:
-    #    print(element)
+# The code used to get the shape of the tensor images
+# Output: shape=(4, 256, 256, 3), dtype=float32 <-- 4 batches, 256 x 256 matrix, 3 channels (rgb)
+# for element in monet_normalized:
+#    print(element)
 
 input_channels = 3
 kernel_size = 3
 output_channels = 64
 
 generator = Generator(input_channels, output_channels, kernel_size)
-
-
-
--- CycleGanProject/cycle_gan_model.py
+++ CycleGanProject/cycle_gan_model.py
@@ -1,28 +1,31 @@
 import torch
-import torch.nn as nn 
-from CycleGanProject import load_and_preprocess_dataset
+import torch.nn as nn
+import torch.nn.functional as F
 
-#Two Generators
-#Composed of three parts: Encode, resnet skip connects, and decoder
-#Note: nn.Sequential is used to sequentially execute layers, NOT for defining layers with specific parameters directly within it. So, I can't put something like: input = nn.Conv2d(input, output_channel, kernel_size)
-class Generator(nn.Module):
-    def __init__(self, input_channel:int, output_channel:int, kernel_size:int):
-        
+
+# Two Generators
+# Composed of three parts: Encode, resnet skip connects, and decoder
+# Note: nn.Sequential is used to sequentially execute layers, NOT for defining layers with specific parameters directly within it. So, I can't put something like: input = nn.Conv2d(input, output_channel, kernel_size)
+
+# Encoder
+class Encoder(nn.Module):
+    def __init__(self, input_channel: int, output_channel: int, kernel_size: int):
         super().__init__()
-    
         self.conv_layer = nn.Sequential(
-                nn.Conv2d(input_channel, output_channel, kernel_size),
-                nn.ReLU(),
-                
-                nn.Conv2d(input_channel, output_channel, kernel_size),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size),
+            nn.Conv2d(input_channel, output_channel, kernel_size),
+            nn.ReLU(),
+            nn.Conv2d(output_channel, output_channel, kernel_size),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size),
+            nn.Conv2d(output_channel, output_channel, kernel_size),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size),
+        )
 
-                nn.Conv2d(input_channel, output_channel, kernel_size),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size)
-    )
-        
+    def forward(self, x):
+        return self.conv_layer(x)
+
+# Resnet Block
 class ResNet(nn.Module):
     def __init__(self, input_channels, output_channels, kernel_size):
         super().__init__()
@@ -32,29 +35,106 @@ class ResNet(nn.Module):
             nn.BatchNorm2d(output_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size),
-    
-            
             nn.Conv2d(output_channels, output_channels, kernel_size),
             nn.BatchNorm2d(output_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size),
-            
             nn.Conv2d(output_channels, output_channels, kernel_size),
             nn.BatchNorm2d(output_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size),
-            
             nn.Conv2d(output_channels, output_channels, kernel_size),
             nn.BatchNorm2d(output_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size),
-            
             nn.Conv2d(output_channels, output_channels, kernel_size),
             nn.BatchNorm2d(output_channels),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size),
-            
-            #Create the add operation for later so that I can add the resnet skip connections later
+        )
+        self.skip_module = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, kernel_size),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size),
         )
 
+    def forward(self, x):
+        return self.module(x) + self.skip_module(x)
+
+# Decoder
+class Decoder(nn.Module):
+    def __init__(self, input_channel, output_channel, kernel_size):
+        super().__init__()
+        self.deconv_layer = nn.Sequential(
+            nn.ConvTranspose2d(input_channel, output_channel, kernel_size),
+            nn.ReLU(),
+            nn.ConvTranspose2d(output_channel, output_channel, kernel_size),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size),
+            nn.ConvTranspose2d(output_channel, output_channel, kernel_size),
+            nn.ReLU(),
+            nn.ConvTranspose2d(output_channel, output_channel, kernel_size),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.deconv_layer(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, input_channel: int, output_channel: int, kernel_size: int):
+        super().__init__()
+
+        self.encoder = Encoder(input_channel, output_channel, kernel_size)
+        self.resnet = ResNet(input_channel, output_channel, kernel_size)
+        self.decoder = Decoder(input_channel, output_channel, kernel_size)
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        resnet = self.resnet(encoded)
+        decoded = self.decoder(resnet)
+        return decoded
+    
+
+#This uses a patchGAN discriminator instead of the usual discriminator
+class Discriminator(nn.Module): 
+    def __init__(self,input_channel:int,output_channel:int ,kernel_size:int):
+        super().__init__()
+
+        self.discriminator = nn.Sequential(
+            nn.Conv2d (input_channel,output_channel,kernel_size),
+            nn.Conv2d (output_channel,output_channel,kernel_size),
+            nn.BatchNorm2d(output_channel),
+            nn.Conv2d (output_channel,output_channel,kernel_size),
+            nn.BatchNorm2d(output_channel),
+            nn.Conv2d (output_channel,output_channel,kernel_size),
+            nn.BatchNorm2d(output_channel),
+            nn.Conv2d (output_channel,output_channel,kernel_size)
+            )
         
+        self.weight_init(mean=0.0, std=0.02)
+
+    def weight_init(self, mean, std):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, mean, std)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, input, label):
+      x = torch.cat([input, label], 1)
+      x = F.leaky_relu(self.discriminator(x), 0.2)
+      x = F.sigmoid(x)
+
+      return x
+
+generatorA = Generator(input_channel, output_channel, kernel_size)
+generatorB = Generator(input_channel, output_channel, kernel_size)
+
+discriminatorA = Discriminator(input_channel, output_channel,kernel_size)
+discriminatorB = Discriminator(input_channel, output_channel,kernel_size)
+
+
+
+
